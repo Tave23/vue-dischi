9b66ebaefbb22ead09eff5ebbc7ab515c6d4b155
Davide Tavella <tavella01@gmail.com>
2021-11-26 16:37:22 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+     Immagine spotify
+  </header>
+</template>
+
+<script>
+export default {
+   // gli si assegna il nome prima di importarlo
+   name: "Header"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
+   
+
+
