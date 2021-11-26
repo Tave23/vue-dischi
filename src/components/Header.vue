4d5feb024e0816fa3de30d1df98820b385b654c8
Davide Tavella <tavella01@gmail.com>
2021-11-26 16:47:46 +0100

    Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
   <header>
-     Immagine spotify
+      <div class="box-logo">
+         <img src="../assets/logo.png" alt="Logo">
+      </div>
   </header>
 </template>
 
@@ -12,6 +14,24 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+@import "../assets/style/vars.scss";
+
+header{
+   height: 75px;
+   width: 100vw;
+   background-color: $header-color;
+   line-height: 75px;
+   padding-left: 10px;
+}
+
+.box-logo{
+   width: 5%;
+}
+
+.box-logo img{
+   width: 100%;
+}
 
 </style>
 
